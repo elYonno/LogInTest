169c4b302d3958a464e21183a1f741d2dc41fc40
elYonno <78961541+elYonno@users.noreply.github.com>
2021-04-11 11:47:37 +0200

    yay

--- app/src/main/java/com/example/logintest/Background/RegisterBackground.java
+++ app/src/main/java/com/example/logintest/Background/RegisterBackground.java
@@ -2,86 +2,100 @@ package com.example.logintest.Background;
 
 import android.os.AsyncTask;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.logintest.UI.Register;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class RegisterBackground extends AsyncTask<String, Void, String> {
+public class RegisterBackground {
 
     private String username;
     private String password;
     private String result;
-    private static final String ADD_URL = "https://studev.groept.be/api/a20sd505/addLogIn/"; // username/email/password
-    private static final String CHECK_URL = "https://studev.groept.be/api/a20sd505/checkLogIn/";
+    private static final String ADD_URL = "https://studev.groept.be/api/a20sd505/addLogIn/";
+    private static final String CHECK_URL = "https://studev.groept.be/api/a20sd505/checkUsernames";
+    RequestQueue requestQueue;
 
-    @Override
-    protected String doInBackground(String... strings) {
-        System.out.println("Register background made!");
+    public RegisterBackground(RequestQueue rq, String... strings) {
         this.username = strings[0];
         this.password = strings[1];
-        result = "";
+        this.result = "Default";
+        this.requestQueue = rq;
 
-//        if (checker())
-        register();
-
-        return null;
+        checker();
     }
 
-    private boolean checker() {
+    private void checker() {
         System.out.println("Checking");
-        AtomicBoolean check = new AtomicBoolean(false);
 
-        new JsonArrayRequest(Request.Method.GET, CHECK_URL, null,
-                response -> {
-                    boolean test = true;
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, CHECK_URL, null,
+            response -> {
+                try {
+                    boolean valid = true;
+
                     for (int i = 0; i < response.length(); i++) {
-                         try {
-                             JSONObject o = response.getJSONObject(i);
+                         System.out.println("Username: " + i);
+                         JSONObject o = response.getJSONObject(i);
 
-                             String temp_name = o.get("Username").toString();
+                         String temp_name = o.get("Username").toString();
+                         System.out.println(temp_name);
 
-                             if (temp_name.equals(username)) {
-                                 test = false;
-                                 result = "Username already exists!";
-                                 break;
-                             }
-                         } catch (JSONException e) {
-                             System.out.println("-------------------------------------ERROR------------------------------------------");
-                             result = e.toString();
-                             e.printStackTrace();
+                         if (temp_name.equals(username)) {
+                             result = "Username already exists!";
+                             valid = false;
+                             break;
                          }
-                     }
-                    check.set(test);
-                }, error -> {
-                    result = "Cannot return the database!";
-                });
-
-        return check.get();
+                    }
+                    if (valid) {
+                        System.out.println("Username doesn't exist, attempting to add.");
+                        register();
+                    }
+                 }
+                catch (JSONException e) {
+                    System.out.println("Error iterating through JSon: " + e.toString());
+                 }
+            },
+            error -> System.out.println("Error array request: " + error.toString())
+        );
+        requestQueue.add(jsonArrayRequest);
     }
 
     private void register() {
-        JSONObject data = new JSONObject();
-        try {
-            data.put("username", username);
-            data.put("password", password);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Adding JSon");
+        StringRequest submitRequest = new StringRequest(Request.Method.POST, ADD_URL,
+                response -> System.out.println("Success: " + response),
+                error -> System.out.println("Error adding JSon: " + error.toString())) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                System.out.println("Adding map");
+                Map<String, String> params = new HashMap<>();
 
-        new JsonObjectRequest(Request.Method.PUT, ADD_URL, data,
-                response -> {
-                    result = "Username " + username + " successfully been created!";
-                }, error -> {
-                    result = "There was an error!";
-        });
+                params.put("username", username);
+                params.put("password", password);
+                result = "Username " + username + " created";
 
+                return params;
+            }
+
+        };
+
+        requestQueue.add(submitRequest);
+    }
+
+    public String getResult() {
+        return result;
     }
 }
--- app/src/main/java/com/example/logintest/UI/Register.java
+++ app/src/main/java/com/example/logintest/UI/Register.java
@@ -1,7 +1,5 @@
 package com.example.logintest.UI;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,24 +8,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
+
 import com.android.volley.toolbox.Volley;
+import com.example.logintest.Background.RegisterBackground;
 import com.example.logintest.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Register extends AppCompatActivity {
 
@@ -35,8 +21,6 @@ public class Register extends AppCompatActivity {
     TextView password;
     Button button;
     RequestQueue requestQueue;
-
-    private static final String ADD_URL = "https://studev.groept.be/api/a20sd505/addLogIn/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,49 +33,21 @@ public class Register extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
     }
 
-    public void onClick_btnRegister(View view) {
-        Toast.makeText(Register.this, "Register button clicked", Toast.LENGTH_LONG).show();
+    public void onClick_btnAdd(View view) {
+        System.out.println("Register button clicked");
         String text_user = username.getText().toString();
         String text_password = password.getText().toString();
 
-//        AsyncTask<String, Void, String> rb = new RegisterBackground().execute(text_user, text_password);
-//
-//        try {
-//            Toast.makeText(this, rb.get(), Toast.LENGTH_LONG);
-//        } catch (Exception e) { e.printStackTrace(); }
-
-        JSONObject data = new JSONObject();
+        RegisterBackground registerBackground = new RegisterBackground(requestQueue, text_user, text_password);
         try {
-            data.put("username", text_user);
-            data.put("password", text_password);
-        } catch (JSONException e) {
-            e.printStackTrace();
+            wait(1000);
         }
+        catch (Exception e) {}
+        toast(registerBackground.getResult());
 
-        StringRequest submitRequest = new StringRequest(Request.Method.POST, ADD_URL, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(Register.this, "Username added", Toast.LENGTH_LONG).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(Register.this, "Error adding username", Toast.LENGTH_LONG).show();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
+    }
 
-                params.put("username", text_user);
-                params.put("password", text_password);
-
-                return params;
-            }
-
-        };
-
-        requestQueue.add(submitRequest);
-
+    public void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
